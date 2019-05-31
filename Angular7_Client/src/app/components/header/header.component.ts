c06ab6602371db4acdc5cfacb311133d8daba211
nmehare <50725648+nmehare@users.noreply.github.com>
2019-05-31 15:54:46 +1000

    Update header.component.ts

--- Angular7_Client/src/app/components/header/header.component.ts
+++ Angular7_Client/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  isactive:boolean;
+  constructor(private userservice: UserService, private router:Router) {   
   }
 
+  ngOnInit() {
+    this.isactive = this.userservice.isLoggedIn();   
+  }
 }
